045bef96df7fd51897ba0ca22cb5a4e1d4af3f9e
AlfredoCastrogiovanni <castro.alfry@gmail.com>
2023-12-18 14:33:37 +0100

    first commit

---
+++ src/components/AppMain.vue
@@ -0,0 +1,18 @@
+<script>
+    import Listcards from './Listcards.vue';
+
+    export default {
+        name: "AppMain",
+        components: {
+            Listcards
+        }
+    }
+</script>
+
+<template>
+    <Listcards/>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
